4aadbc4a91910662e7eac041688910c9f318ff6b
RPS98 <rafa.perez.s98@gmail.com>
2022-10-06 17:52:03 +0200

    Change ownSendCommand to pure virtual

--- include/as2_core/aerial_platform.hpp
+++ include/as2_core/aerial_platform.hpp
@@ -109,10 +109,7 @@ public:
    * @return true command is sended successfully.
    * @return false command is not sended.
    */
-  virtual bool ownSendCommand() {
-    RCLCPP_FATAL(get_logger(), "ownSendCommand() not implemented");
-    return false;
-  }
+  virtual bool ownSendCommand() = 0;
 
   /**
    * @brief Handles how arming state has to be settled  in the concrete platform.
